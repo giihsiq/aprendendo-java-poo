c6d12eb51ce781fe738604519b6f6cc2f58fb9e8
Giovanna Siqueira <giihsiq.contato@gmail.com>
2026-02-24 16:20:23 -0300

    feat: Segundo exemplo utilizando Orientação a Objetos + método toString()

--- src/udemy/devsuperior/poo/exemplo/Program.java
+++ src/udemy/devsuperior/poo/exemplo/Program.java
@@ -10,33 +10,39 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner scanner = new Scanner(System.in);
 		
-		Triangle x, y;
-		x = new Triangle();
-		y = new Triangle();
+		Products products = new Products();
 		
-		System.out.println("Enter the measures of triangle X:");
-		x.a = scanner.nextDouble();
-		x.b = scanner.nextDouble();
-		x.c = scanner.nextDouble();
+		System.out.println("Enter product data:");
+		System.out.print("Name: ");
+		products.name = scanner.nextLine();
 		
-		System.out.println("Enter the measures of triangle Y:");
-		y.a = scanner.nextDouble();
-		y.b = scanner.nextDouble();
-		y.c = scanner.nextDouble();
+		System.out.print("Price: ");
+		products.price = scanner.nextDouble();
 		
-		double areaX = x.area();
-		double areaY = y.area();
+		System.out.print("Quantity: ");
+		products.quantity = scanner.nextInt();
 		
-		System.out.printf("Triangle X area: %.4f%n", areaX);
-		System.out.printf("Triangle Y area: %.4f%n", areaY);
+		System.out.println();
+		System.out.println("Products data " + products);
 		
-		if (areaX > areaY) {
-			System.out.println("Larger area: X");
-		} else {
-			System.out.println("Larger area: Y");
-		}
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = scanner.nextInt();
+		products.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data " + products);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be removed in stock: ");
+		quantity = scanner.nextInt();
+		products.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Udapted data " + products);
 		
 		scanner.close();
 		
 	}
+
 }
