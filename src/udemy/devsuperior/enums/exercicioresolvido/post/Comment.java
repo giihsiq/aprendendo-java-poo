12103da14e80c1543d7ba99d94d30bc543a43225
Giovanna Siqueira <giihsiq.contato@gmail.com>
2026-03-11 17:12:43 -0300

    feat: Exercício resolvido (enums e composition)

---
+++ src/udemy/devsuperior/enums/exercicioresolvido/post/Comment.java
@@ -0,0 +1,22 @@
+package udemy.devsuperior.enums.exercicioresolvido.post;
+
+public class Comment {
+	
+	private String text;
+	
+	public Comment() {
+	}
+
+	public Comment(String text) {
+		this.text = text;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
